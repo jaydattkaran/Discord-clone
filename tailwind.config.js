d1ba1a35b8b548f8ed5dc2c718bbc0bce398cbd8
jaydattkaran <karanjaydatt03@gmail.com>
2023-09-20 22:26:50 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        
+        Whitney:["Whiteney"],
+        Ginto:["Ginto"],
+        ggsans: ['ggsans-normal'],
+      },
+      colors:{
+        lightBlue : "#404EED"
+
+      }
+    },
+  },
+  plugins: [],
+}
+
